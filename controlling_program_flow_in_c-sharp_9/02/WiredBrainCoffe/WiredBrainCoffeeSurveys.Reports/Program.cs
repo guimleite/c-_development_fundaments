c9a17565a317f3ac9222a67caa7a54b9adbe80ed
Guilherme <guilhermemleitte@hotmail.com>
2022-12-05 10:06:37 -0300

    Exercices of module 2 of the course

---
+++ controlling_program_flow_in_c-sharp_9/02/WiredBrainCoffe/WiredBrainCoffeeSurveys.Reports/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WiredBrainCoffeeSurveys.Reports
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Calculated values os Q1 Results
+            double overallScore = Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.FoodScore + Q1Results.PriceScore / 4;
+            double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
+            double unansweredCount = Q1Results.NumberSurveyed - Q1Results.NumberResponded;                        
+            
+            Console.WriteLine($"Reponse Percentage: {responseRate}");
+            Console.WriteLine($"Unanswered Surveys: {unansweredCount}");
+            Console.WriteLine($"Overall Score: {overallScore}");
+
+            bool higherCoffeScore = Q1Results.CoffeeScore > Q1Results.FoodScore;
+            bool customersRecommend = Q1Results.WouldRecommend >= 7;
+            bool noGranolaYesCappucino = Q1Results.LeastFavoriteProduct == "Granola" && Q1Results.FavoriteProduct == "Cappucino";
+
+            Console.WriteLine($"Coffe Score Higher Than Food: {higherCoffeScore}");
+            Console.WriteLine($"Customers Would Recommend Us: {customersRecommend}");
+            Console.WriteLine($"Hate Granola, love Cappucino: {noGranolaYesCappucino}");
+
+        }
+    }
+}
